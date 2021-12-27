8beb54703ad942050e2f92dd882fad6f22b4945f
Alex Dudescu <alexdudescu@gmail.com>
2021-12-27 17:12:07 +0200

    feat: implement soundboard logic

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    document.querySelector("body").classList.add("bg-slate-100")
+  });
+
   return <Component {...pageProps} />
 }
 
